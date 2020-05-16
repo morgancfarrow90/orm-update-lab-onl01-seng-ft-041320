85d54b5991811c497f362e0ca3bcd19e54f05a60
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 17:03:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ def save
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
-end
+
   
     def self.create(name:, grade:)
     student = self.new(name, grade)
