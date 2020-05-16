bfb70d97ad1604d49a02d2cd5b708b1e0ac30571
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 17:15:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ end
     new_student = self.new
     new_student.id = row[0]
     new_student.name =  row[1]
-    new_student.length = row[2]
+    new_student.grade = row[2]
     new_student  
   end
 
