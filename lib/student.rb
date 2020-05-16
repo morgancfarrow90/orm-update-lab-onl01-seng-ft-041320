de4d5e8dda754402c46c967580b24fadc724d38d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 17:34:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,16 +68,10 @@ end
   end
   
   def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT * 
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-    SQL
-    
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
   end
+
 
 end
