a4645349de85563eb1ebf7edd0411615fe3ea5a7
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 17:19:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-def initialize(name, grade, id=NIL)
+def initialize(id= NIL, name, grade)
   @name = name
   @grade = grade
   @id = id
